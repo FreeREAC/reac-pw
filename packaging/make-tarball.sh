67df8d4c7011ac88676a09363a400040e7514485
Pau Aliagas <linuxnow@gmail.com>
2026-08-23 22:26:43 +0200

    packaging: the tarball ships tools/, which meson.build references and the build needs

--- packaging/make-tarball.sh
+++ packaging/make-tarball.sh
@@ -17,7 +17,7 @@ V="${1:-$MESON_V}"
 
 T=$(mktemp -d); D="$T/reac-pw-$V"
 mkdir -p "$D"
-rsync -a --exclude '.git' --exclude 'build' "$ROOT/src" "$ROOT/tests" \
+rsync -a --exclude '.git' --exclude 'build' "$ROOT/src" "$ROOT/tests" "$ROOT/tools" \
       "$ROOT/meson.build" "$ROOT/meson_options.txt" "$ROOT/LICENSE" "$ROOT/README.md" \
       "$ROOT/packaging" "$D/"
 tar -czf "$ROOT/reac-pw-$V.tar.gz" -C "$T" "reac-pw-$V"
